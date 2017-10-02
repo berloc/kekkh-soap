6a31771392b2fcae4cfa6fcb5e89b727168bd0cb
berloc <berloc1987@gmail.com>
2017-10-02 11:28:23 +0200

    initial commit

---
+++ src/main/java/com/vodafone/KekkhmockApplication.java
@@ -0,0 +1,12 @@
+package com.vodafone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekkhmockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KekkhmockApplication.class, args);
+	}
+}
