c4b49c86257435e13c6dc94b211f4d92abd2c951
berloc <berloc1987@gmail.com>
2017-10-10 16:02:09 +0200

    setup to run on external tomcat

--- src/main/java/com/vodafone/KekkhmockApplication.java
+++ src/main/java/com/vodafone/KekkhmockApplication.java
@@ -2,13 +2,16 @@ package com.vodafone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KekkhmockApplication {
+public class KekkhmockApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KekkhmockApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KekkhmockApplication.class, args);
